7ec5957c4cc1b327aa40c5d1c4852c2db0ded136
aashay <aashay_thakkar@hotail.com>
2026-02-12 16:43:40 +0530

    Remove Core Services section from services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,27 +1,9 @@
 import ServicesHero from '@/components/services/ServicesHero';
 import ServicesIntro from '@/components/services/ServicesIntro';
 import KeyExpertise from '@/components/services/KeyExpertise';
-import CoreServices from '@/components/services/CoreServices';
-import ProjectDelivery from '@/components/services/ProjectDelivery';
-import Differentiation from '@/components/services/Differentiation';
-import AdditionalServices from '@/components/services/AdditionalServices';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import FinalCTA from '@/components/about/FinalCTA';
-
-
-export default function ServicesPage() {
-    return (
-        <main className="bg-bg-off-white min-h-screen">
-            <Navbar />
-
-            <ServicesHero />
-
-            <ServicesIntro />
-
             <KeyExpertise />
 
-            <CoreServices />
+            <ProjectDelivery />
 
             <ProjectDelivery />
 
@@ -34,6 +16,6 @@ export default function ServicesPage() {
 
 
             <Footer />
-        </main>
+        </main >
     );
 }
