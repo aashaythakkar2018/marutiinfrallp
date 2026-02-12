a78332ba2391ed50ddfa9b7fa15da6b7e40a835c
aashay <aashay_thakkar@hotail.com>
2026-02-12 16:44:37 +0530

    Fix syntax errors in services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,24 @@
 import ServicesHero from '@/components/services/ServicesHero';
 import ServicesIntro from '@/components/services/ServicesIntro';
 import KeyExpertise from '@/components/services/KeyExpertise';
-            <KeyExpertise />
+import ProjectDelivery from '@/components/services/ProjectDelivery';
+import Differentiation from '@/components/services/Differentiation';
+import AdditionalServices from '@/components/services/AdditionalServices';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import FinalCTA from '@/components/about/FinalCTA';
 
-            <ProjectDelivery />
+
+export default function ServicesPage() {
+    return (
+        <main className="bg-bg-off-white min-h-screen">
+            <Navbar />
+
+            <ServicesHero />
+
+            <ServicesIntro />
+
+            <KeyExpertise />
 
             <ProjectDelivery />
 
@@ -13,9 +28,7 @@ import KeyExpertise from '@/components/services/KeyExpertise';
 
             <FinalCTA />
 
-
-
             <Footer />
-        </main >
+        </main>
     );
 }
