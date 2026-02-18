f8aa103ba6ac38d6b91c8b0d821bc3759f9fc65a
aashay <aashay_thakkar@hotail.com>
2026-02-18 13:22:16 +0530

    Fix hero video stopping - add playsinline=1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
             {/* Background Video Layer */}
             <div className="absolute inset-0 z-0 pointer-events-none">
                 <iframe
-                    src="https://www.youtube.com/embed/YlUthMUU-RQ?autoplay=1&mute=1&controls=0&loop=1&playlist=YlUthMUU-RQ&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&vq=hd1080"
+                    src="https://www.youtube.com/embed/YlUthMUU-RQ?autoplay=1&mute=1&controls=0&loop=1&playlist=YlUthMUU-RQ&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&vq=hd1080&playsinline=1"
                     className="absolute top-1/2 left-1/2 w-[177.78vh] min-w-full min-h-[56.25vw] h-[56.25vw] -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
