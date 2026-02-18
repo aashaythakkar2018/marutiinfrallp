e6260477a985b7462766903e21cae453677ff955
aashay <aashay_thakkar@hotail.com>
2026-02-18 13:12:49 +0530

    Fix hero video distortion and pixelation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,8 +11,8 @@ export default function Hero() {
             {/* Background Video Layer */}
             <div className="absolute inset-0 z-0 pointer-events-none">
                 <iframe
-                    src="https://www.youtube.com/embed/YlUthMUU-RQ?autoplay=1&mute=1&controls=0&loop=1&playlist=YlUthMUU-RQ&showinfo=0&rel=0&iv_load_policy=3&disablekb=1"
-                    className="absolute top-1/2 left-1/2 w-[300%] h-[300%] -translate-x-1/2 -translate-y-1/2 min-w-full min-h-full object-cover pointer-events-none"
+                    src="https://www.youtube.com/embed/YlUthMUU-RQ?autoplay=1&mute=1&controls=0&loop=1&playlist=YlUthMUU-RQ&showinfo=0&rel=0&iv_load_policy=3&disablekb=1&vq=hd1080"
+                    className="absolute top-1/2 left-1/2 w-[177.78vh] min-w-full min-h-[56.25vw] h-[56.25vw] -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 />
