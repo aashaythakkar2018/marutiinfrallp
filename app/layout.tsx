7b1e2a28a687745a3cc9f075e016293cb9b08504
aashay <aashay_thakkar@hotail.com>
2026-08-31 13:07:07 +0530

    feat: Add Google Tag Manager (GTM-K4RQDW95)

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="-JxH088o9vX52lpujheKTnC_bvGm5wxuR-3zOsv42JY" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K4RQDW95');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-KCZ5QQ7EGJ"
@@ -68,6 +79,16 @@ export default function RootLayout({
       <body
         className={`${outfit.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4RQDW95"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
