3ca0eb229e9abbf6f625219f4a7d648cf29c7b0e
aashay <aashay_thakkar@hotail.com>
2026-08-31 14:13:49 +0530

    fix: Embed GTM script directly in layout head for static HTML rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,30 +51,32 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="-JxH088o9vX52lpujheKTnC_bvGm5wxuR-3zOsv42JY" />
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K4RQDW95');
-          `}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K4RQDW95');`,
+          }}
+        />
         {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
-        <Script
+        <script
+          async
           src="https://www.googletagmanager.com/gtag/js?id=G-KCZ5QQ7EGJ"
-          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', 'G-KCZ5QQ7EGJ');
-          `}
-        </Script>
+            `,
+          }}
+        />
       </head>
       <body
         className={`${outfit.variable} ${geistMono.variable} antialiased`}
